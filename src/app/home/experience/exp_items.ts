6bc15e052028e31ccda505599c0476c00be0ef08
Ben Albritton <benalbritton@outlook.com>
2018-05-31 21:10:00 -0500

    added section

---
+++ src/app/home/experience/exp_items.ts
@@ -0,0 +1,17 @@
+import { Exp } from './exp';
+
+export const EXPS: Exp[] = [
+    {item: "Microsoft SQL Server"},
+    {item: "Splunk"},
+    {item: "Dell KACE SQL Reports"},
+    {item: "Angular"},
+    {item: "Pervasive/Actian Data Connect"},
+    {item: "Microsoft Active Directory"}
+];
+
+
+// Microsoft SQL Server
+// Splunk
+// Dell KACE SQL Reports
+// Pervasive/Actian Data Connect
+// Microst Active Directory
--- src/app/home/experience/experience.component.ts
+++ src/app/home/experience/experience.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { EXPS } from './exp_items';
+
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['../../app.component.css']
 })
 export class ExperienceComponent implements OnInit {
 
+  exps = EXPS;
   constructor() { }
 
   ngOnInit() {
